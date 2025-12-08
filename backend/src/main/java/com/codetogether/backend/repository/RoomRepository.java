1c98895a2d66c125c2f9936d09594a0afaef7c18
Rohith1905 <rohithadiga19@gmail.com>
2025-12-08 22:43:00 +0530

    feat: create repositories for Room and File

---
+++ backend/src/main/java/com/codetogether/backend/repository/RoomRepository.java
@@ -0,0 +1,13 @@
+package com.codetogether.backend.repository;
+
+import com.codetogether.backend.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, UUID> {
+    List<Room> findByCreatedBy_Id(UUID userId);
+}
